2a5fe82c769b484cbde6a277e529fb44d4b94e7f
ali-mashayekhi <alimashayekhi42@gmail.com>
2023-08-04 11:24:59 +0330

    newExpense component added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -6,13 +6,23 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  console.log(`ExpenseItem evaluated by react ${props.title}`);
+
+  function clickHandler() {
+    setTitle("Changed");
+    return console.log(title);
+    // return title === "Changed" ? setTitle("Toilet Paper") : setTitle("Changed");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
