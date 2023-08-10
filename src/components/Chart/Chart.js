d99329dc0c59409c0d4371328ba8f242dd4d5094
ali-mashayekhi <alimashayekhi42@gmail.com>
2023-08-10 08:18:43 +0330

    filter feature added

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CharBars from "./CharBars";
+
+import "./Chart.css";
+
+function Chart(props) {}
+
+export default Chart;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -6,24 +6,16 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  console.log(`ExpenseItem evaluated by react ${props.title}`);
-
-  function clickHandler() {
-    setTitle("Changed");
-    return console.log(title);
-    // return title === "Changed" ? setTitle("Toilet Paper") : setTitle("Changed");
-  }
-
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 }
 
