526c7e62ed7140867961ca6ae23aa5078062b456
ali-mashayekhi <alimashayekhi42@gmail.com>
2023-08-11 11:35:24 +0330

    char added, app finished

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,25 @@
 import React from "react";
-import CharBars from "./CharBars";
+import ChartBars from "./ChartBars";
+import Card from "../UI/Card";
 
 import "./Chart.css";
 
-function Chart(props) {}
+function Chart(props) {
+  const values = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const maxValue = Math.max(...values);
+
+  return (
+    <Card className="chart">
+      {props.dataPoints.map((dataPoints) => (
+        <ChartBars
+          key={dataPoints.label}
+          value={dataPoints.value}
+          maxValue={maxValue}
+          label={dataPoints.label}
+        />
+      ))}
+    </Card>
+  );
+}
 
 export default Chart;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -23,6 +24,7 @@ function Expenses(props) {
         selected={filteredYear}
         onFilterChange={getFilterHandler}
       />
+      <ExpensesChart items={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
